a28ef8037b49f62fa399476541c3d5aa5bead5e7
Eric Carles <carles.eric@wanadoo.fr>
2017-11-28 18:40:26 +0000

    TestRiver test_remove_fish_river_full test_remove_fish_river_full passed

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -12,6 +12,8 @@ class TestRiver < Minitest::Test
     @fish5 = Fish.new("Torpedo")
     @fish  = [@fish1, @fish2, @fish3, @fish4, @fish5]
     @river = River.new("Amazon", @fish)
+
+    @river_empty = River.new("Amazon", [])
   end
 
   def test_get_fish
@@ -22,10 +24,16 @@ class TestRiver < Minitest::Test
     assert_equal(5, @river.get_nb_fish())
   end
 
-  def test_remove_fish
+  def test_remove_fish_river_full
     fish_caught = @river.remove_fish()
     assert_equal(@fish5, fish_caught)
     assert_equal(4, @river.get_nb_fish)
+  end
+
+  def test_remove_fish_river_empty
+    fish_caught = @river_empty.remove_fish()
+    assert_nil(fish_caught)
+    assert_equal(0, @river_empty.get_nb_fish)
   end
 
 end
